5fcd7c5d39ddcf1e321f504dcf9f97afdb490165
hrazafin <herilala.razafi@gmail.com>
2024-07-10 08:07:21 -0500

    setup changed and adding XSec analysis

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 echo calling DUNEprotoTKI/setup.sh 
 
+source DUNE.sh
 export DUNEPROTOTKI=$(pwd)
 export LOCALBIN=${DUNEPROTOTKI}/bin
 export ROOUNFOLD=${DUNEPROTOTKI}/RooUnfold/build
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${ROOUNFOLD}
 export PATH=$PATH:${LOCALBIN}
-source DUNE.sh
+#source DUNE.sh
